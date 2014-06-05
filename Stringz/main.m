9a4c61b96ea6a948283700d8140c6ce379fa43fe
Keith Samson <k.samson@klab.com>
2014-06-05 09:56:48 +0800

    Stringz's first commit

---
+++ Stringz/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Stringz
+//
+//  Created by Keith Samson on 6/5/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Writing Files with NSString and NSData");
+        
+    }
+    return 0;
+}
+
